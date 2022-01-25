0b96a9a624186102ecd9b9fb614397fd65a232c6
drlight17 <37434652+drlight17@users.noreply.github.com>
2022-01-25 17:07:25 +0300

    some fixes
    Added mail attribute instead of userPrincipal
    Added exception handler in sync() function to process ignoring some indexes error

--- syncer.py
+++ syncer.py
@@ -9,8 +9,8 @@ from pathlib import Path
 import logging
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d.%m.%y %H:%M:%S', level=logging.INFO)
 
-def main():    
-    global config 
+def main():
+    global config
     config = read_config()
 
     passdb_conf = read_dovecot_passdb_conf_template()
@@ -34,57 +34,60 @@ def main():
         time.sleep(interval)
 
 def sync():
-    ldap_connector = ldap.initialize(f"{config['LDAP_URI']}")
-    ldap_connector.set_option(ldap.OPT_REFERRALS, 0)
-    ldap_connector.simple_bind_s(config['LDAP_BIND_DN'], config['LDAP_BIND_DN_PASSWORD'])
+    try:
+        ldap_connector = ldap.initialize(f"{config['LDAP_URI']}")
+        ldap_connector.set_option(ldap.OPT_REFERRALS, 0)
+        ldap_connector.simple_bind_s(config['LDAP_BIND_DN'], config['LDAP_BIND_DN_PASSWORD'])
 
-    ldap_results = ldap_connector.search_s(config['LDAP_BASE_DN'], ldap.SCOPE_SUBTREE, 
-                config['LDAP_FILTER'], 
-                ['mail', 'cn', 'USERACCOUNTCONTROL'])
+        ldap_results = ldap_connector.search_s(config['LDAP_BASE_DN'], ldap.SCOPE_SUBTREE,
+                    config['LDAP_FILTER'],
+                    ['mail', 'cn', 'userAccountControl'])
 
-    ldap_results = map(lambda x: (
-        x[1]['mail'][0].decode(),
-        x[1]['cn'][0].decode(),
-        False if int(x[1]['USERACCOUNTCONTROL'][0].decode()) & 0b10 else True), ldap_results)
-    logging.info (f"DrLight int(x[1]: {int(x[1])}")
+        ldap_results = map(lambda x: (
+            x[1]['mail'][0].decode(),
+            x[1]['cn'][0].decode(),
+            False if int(x[1]['userAccountControl'][0].decode()) & 0b10 else True), ldap_results)
 
-    filedb.session_time = datetime.datetime.now()
+        filedb.session_time = datetime.datetime.now()
 
-    for (email, ldap_name, ldap_active) in ldap_results:
-        (db_user_exists, db_user_active) = filedb.check_user(email)
-        (api_user_exists, api_user_active, api_name) = api.check_user(email)
+        for (email, ldap_name, ldap_active) in ldap_results:
 
-        unchanged = True
+                (db_user_exists, db_user_active) = filedb.check_user(email)
+                (api_user_exists, api_user_active, api_name) = api.check_user(email)
 
-        if not db_user_exists:
-            filedb.add_user(email, ldap_active)
-            (db_user_exists, db_user_active) = (True, ldap_active)
-            logging.info (f"Added filedb user: {email} (Active: {ldap_active})")
-            unchanged = False
+                unchanged = True
 
-        if not api_user_exists:
-            api.add_user(email, ldap_name, ldap_active)
-            (api_user_exists, api_user_active, api_name) = (True, ldap_active, ldap_name)
-            logging.info (f"Added Mailcow user: {email} (Active: {ldap_active})")
-            unchanged = False
+                if not db_user_exists:
+                    filedb.add_user(email, ldap_active)
+                    (db_user_exists, db_user_active) = (True, ldap_active)
+                    logging.info (f"Added filedb user: {email} (Active: {ldap_active})")
+                    unchanged = False
 
-        if db_user_active != ldap_active:
-            filedb.user_set_active_to(email, ldap_active)
-            logging.info (f"{'Activated' if ldap_active else 'Deactived'} {email} in filedb")
-            unchanged = False
+                if not api_user_exists:
+                    api.add_user(email, ldap_name, ldap_active)
+                    (api_user_exists, api_user_active, api_name) = (True, ldap_active, ldap_name)
+                    logging.info (f"Added Mailcow user: {email} (Active: {ldap_active})")
+                    unchanged = False
 
-        if api_user_active != ldap_active:
-            api.edit_user(email, active=ldap_active)
-            logging.info (f"{'Activated' if ldap_active else 'Deactived'} {email} in Mailcow")
-            unchanged = False
+                if db_user_active != ldap_active:
+                    filedb.user_set_active_to(email, ldap_active)
+                    logging.info (f"{'Activated' if ldap_active else 'Deactived'} {email} in filedb")
+                    unchanged = False
 
-        if api_name != ldap_name:
-            api.edit_user(email, name=ldap_name)
-            logging.info (f"Changed name of {email} in Mailcow to {ldap_name}")
-            unchanged = False
+                if api_user_active != ldap_active:
+                    api.edit_user(email, active=ldap_active)
+                    logging.info (f"{'Activated' if ldap_active else 'Deactived'} {email} in Mailcow")
+                    unchanged = False
 
-        if unchanged:
-            logging.info (f"Checked user {email}, unchanged")
+                if api_name != ldap_name:
+                    api.edit_user(email, name=ldap_name)
+                    logging.info (f"Changed name of {email} in Mailcow to {ldap_name}")
+                    unchanged = False
+
+                if unchanged:
+                    logging.info (f"Checked user {email}, unchanged")
+    except Exception:
+        pass
 
     for email in filedb.get_unchecked_active_users():
         (api_user_exists, api_user_active, _) = api.check_user(email)
@@ -92,7 +95,7 @@ def sync():
         if (api_user_active and api_user_active):
             api.edit_user(email, active=False)
             logging.info (f"Deactivated user {email} in Mailcow, not found in LDAP")
-        
+
         filedb.user_set_active_to(email, False)
         logging.info (f"Deactivated user {email} in filedb, not found in LDAP")
 
@@ -117,18 +120,18 @@ def apply_config(config_file, config_data):
     Path(os.path.dirname(config_file)).mkdir(parents=True, exist_ok=True)
 
     print(config_data, file=open(config_file, 'w'))
-    
+
     logging.info(f"Saved generated config file to {config_file}")
     return True
 
 def read_config():
     required_config_keys = [
-        'LDAP-MAILCOW_LDAP_URI', 
+        'LDAP-MAILCOW_LDAP_URI',
         'LDAP-MAILCOW_LDAP_BASE_DN',
-        'LDAP-MAILCOW_LDAP_BIND_DN', 
+        'LDAP-MAILCOW_LDAP_BIND_DN',
         'LDAP-MAILCOW_LDAP_BIND_DN_PASSWORD',
-        'LDAP-MAILCOW_API_HOST', 
-        'LDAP-MAILCOW_API_KEY', 
+        'LDAP-MAILCOW_API_HOST',
+        'LDAP-MAILCOW_API_KEY',
         'LDAP-MAILCOW_SYNC_INTERVAL'
     ]
 
@@ -156,7 +159,7 @@ def read_dovecot_passdb_conf_template():
         data = Template(f.read())
 
     return data.substitute(
-        ldap_uri=config['LDAP_URI'], 
+        ldap_uri=config['LDAP_URI'],
         ldap_base_dn=config['LDAP_BASE_DN']
         )
 
@@ -165,7 +168,7 @@ def read_sogo_plist_ldap_template():
         data = Template(f.read())
 
     return data.substitute(
-        ldap_uri=config['LDAP_URI'], 
+        ldap_uri=config['LDAP_URI'],
         ldap_base_dn=config['LDAP_BASE_DN'],
         ldap_bind_dn=config['LDAP_BIND_DN'],
         ldap_bind_dn_password=config['LDAP_BIND_DN_PASSWORD'],
