da33351efb2a28c9dcbdba459938b5698eb4d713
drlight17 <37434652+drlight17@users.noreply.github.com>
2024-01-31 14:22:02 +0300

    Update 014-custom.js

--- data/web/js/build/014-custom.js
+++ data/web/js/build/014-custom.js
@@ -1,7 +1,10 @@
 $( document ).ready(function() {
-  // disable quota and username inputs
+  // disable quota, username,custom attrs inputs
     $("#collapse-tab-medit > form > div:nth-child(5) > div").addClass("custom_disabled_div");
     $("#collapse-tab-medit > form > div:nth-child(7) > div").addClass("custom_disabled_div");
+    $("#collapse-tab-mattr > form tr").addClass("custom_disabled_div");
     $(".custom_disabled_div input[name='name']").attr('tabindex', '-1');
     $(".custom_disabled_div input[name='quota']").attr('tabindex', '-1');
+    $(".custom_disabled_div input[data-id='mbox_attr']").attr('tabindex', '-1');
 });
+
